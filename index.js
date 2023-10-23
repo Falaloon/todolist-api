e8a32ea569ebe4c495b3eb6962c53b74169fdc34
Falaloon <sr.ratchatapusit@gmail.com>
2023-10-23 15:11:23 +0700

    Change from mjs to js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-import "../loadEnvironment.mjs";
+import "./loadEnvironment.js";
 import "express-async-errors";
-import todo from "../routes/todo.mjs";
+import todo from "./routes/todo.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
